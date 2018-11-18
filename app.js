fa4722d7baa03d20b25368d425be4e5c3843311e
unknown <you@example.com>
2018-11-18 03:18:18 -0500

    Progressing on Milestone 2 First

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 require('./db');
+require('./auth');
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
 const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//schema models
+User = mongoose.model('User');
+const Course = mongoose.model('Courses');
 
 const app = express();
 
@@ -11,7 +22,7 @@ const session = require('express-session');
 const sessionOptions = {
     secret: 'secret cookie thang (store this elsewhere!)',
     resave: true,
-      saveUninitialized: true
+    saveUninitialized: true
 };
 app.use(session(sessionOptions));
 
@@ -25,8 +36,71 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// middleware for passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// drop req.user into the context of every template by adding properties to res.locals
+app.use(function(req, res, next){
+	res.locals.user = req.user;
+	next();
+});
+
+app.use('/', routes);
+
+
+//ah4005: linserv1.cims.nyu.edu ... port 20110
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+
+
+// handling login
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+
+// handling register
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+
+app.get('/view', (req, res) => {
+  Course.find({},function(err, result) {
+    //console.log(result);
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render("view", {
+        courses: result
+      });
+    }
+  });
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+app.post('/add', (req, res) => {
+  new Course({
+    name: req.body.name,
+    dates: req.body.dates,
+    rank: req.body.rank,
+    semester: req.body.semester
+  }).save(function(err){
+    if(err){
+      res.render( 'add', {message: "ERROR IN ADDING"});
+    }
+    else{
+      res.redirect('/view');
+    }
+  });
 });
 
 app.listen(3000);
--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-// 1ST DRAFT DATA MODEL
-const mongoose = require('mongoose');
+/*
 
 // users
 // * users must login so they need authentication
 // * users have a username and password and lists (can make more than one)
 const User = new mongoose.Schema({
-  // usernam
-  // password will be hashed
+  username: String,
+  password: {type: String, unique: true, required: true},
   lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
 });
 mongoose.model("Users", User);
@@ -29,6 +28,33 @@ const Course = new mongoose.Schema({
   name: {type: String, required: true},
   dates: {type: String, required: true},
   rank: {type: Number, min: 1, required: true}
+});
+mongoose.model("Courses", Course);
+
+//add the code that connects to the database. We'll connect to the local
+//instance of MongoDB, and we'll use a database called final-proj
+mongoose.connect('mongodb://localhost/final-proj');
+*/
+
+
+
+// 2ND DRAFT DATA MODEL
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema({ });
+UserSchema.plugin(passportLocalMongoose);
+mongoose.model('User', UserSchema);
+
+// course in semester plan
+// * must include name of course, dates (like M/W or T/TR), and rank
+// * items can be filtered by rank
+const Course = new mongoose.Schema({
+  //list: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
+  name: {type: String, required: true},
+  dates: {type: String, required: true},
+  rank: {type: Number, min: 1, required: true},
+  semester: {type: String, required: true}
 });
 mongoose.model("Courses", Course);
 
